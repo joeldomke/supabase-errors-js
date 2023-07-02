734dee2dc79a6339448df406d52bafe51e641298
Joel Domke <joeldomke@aol.de>
2023-07-02 23:58:48 +0200

    Complete sign in with password error code list

--- src/error-handlers/sign-in-with-password.ts
+++ src/error-handlers/sign-in-with-password.ts
@@ -10,16 +10,22 @@ export type SignInWithPasswordErrorCode =
   | 'database-error'
   | 'invalid-login-credentials'
   | 'email-not-confirmed'
-  | 'phone-number-not-confirmed';
+  | 'phone-number-not-confirmed'
+  | 'error-generating-jwt-token';
 
 const signInWithPasswordErrorMap: ErrorMap<SignInWithPasswordErrorCode> = {
   'only-email-or-phone-number': ['Only an email address or phone number should be provided on login.'],
   'email-login-disabled': ['Email logins are disabled'],
   'phone-number-login-disabled': ['Phone logins are disabled'],
-  'database-error': ['Database error querying schema'],
+  'database-error': [
+    'Database error querying schema',
+    'Database error creating audit log entry',
+    'error finding session',
+  ],
   'invalid-login-credentials': ['Invalid login credentials'],
   'email-not-confirmed': ['Email not confirmed'],
   'phone-number-not-confirmed': ['Phone not confirmed'],
+  'error-generating-jwt-token': ['error generating jwt token'],
 };
 
 export function handleSignInWithPasswordError(error: AuthError, handlers: ErrorHandler<SignInWithPasswordErrorCode>) {
